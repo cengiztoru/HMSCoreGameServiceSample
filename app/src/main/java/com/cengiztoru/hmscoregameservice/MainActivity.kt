d268ce07844e251d85794c2863ac84b3f7d4e539
Cengiz TORU <cengiztoru@gmail.com>
2021-10-26 13:25:55 +0300

    [HMS CORE GAME SERVICE] Initialization to game service sdk via user interaction completed

--- app/src/main/java/com/cengiztoru/hmscoregameservice/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscoregameservice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cengiztoru.hmscoregameservice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.hmscoregameservice.databinding.ActivityMainBinding
 import com.huawei.hmf.tasks.Task
 import com.huawei.hms.jos.AppParams
 import com.huawei.hms.jos.JosApps
@@ -12,17 +13,22 @@ import com.huawei.hms.support.account.request.AccountAuthParams
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private val TAG = "MainActivity"
+        private val TAG = "MainActivityTAG"
     }
+
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
-        initSdk()
+        setListeners()
+//        initGameServiceSdk()      // Must be called upon app launch, rather than during user operations such as sign-in and payment.
     }
 
-    private fun initSdk() {
+
+    private fun initGameServiceSdk() {
         val params = AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM_GAME
         val appsClient = JosApps.getJosAppsClient(this)
         val initTask: Task<Void> = appsClient.init(AppParams(params) {
@@ -32,7 +38,16 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener { e -> printLog("init failed, " + e.message) }
     }
 
+
+    private fun setListeners() {
+        //this is just for sample. You should call initialization when app launched
+        mBinding.btnInit.setOnClickListener {
+            initGameServiceSdk()
+        }
+    }
+
     private fun printLog(message: String) {
+        mBinding.tvLogger.append(message + "\n\n")
         Log.i(TAG, message)
     }
 }
